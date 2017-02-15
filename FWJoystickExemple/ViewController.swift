85ee0643d4fc48f4e646be5720c5ccb87f7f9a7f
Keuwa <vincent.durpoix@gmail.com>
2017-02-15 22:05:59 +0100

    test over

--- FWJoystickExemple/ViewController.swift
+++ FWJoystickExemple/ViewController.swift
@@ -11,29 +11,51 @@ import FWJoystick
 
 class ViewController: UIViewController {
     
+    var label: UITextField?;
+    let joystick = FWJoystick();
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        label =  UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label?.textColor = UIColor.blue
+        label?.textAlignment = NSTextAlignment.center
+        label?.text = "Hello"
+        label?.accessibilityLabel = "testLabel";
         
-        let joystick = FWJoystick()
         joystick.actionHandler = { section in
             switch section {
             case 0:
-                print("LEFT")
+                print("LEFT");
+                self.setTextLabel(text: "LEFT")
             case 1:
-                print("UP")
+                self.setTextLabel(text: "UP")
             case 2:
-                print("RIGHT")
+                self.setTextLabel(text: "RIGHT")
             case 3:
-                print("DOWN")
+                self.setTextLabel(text: "DOWN")
             default:
                 break;
-                
             }
         }
         joystick.center = CGPoint(x: view.frame.size.width  / 2,y: view.frame.size.height / 2);
+        joystick.accessibilityLabel = "joystick";
+        label?.center = CGPoint(x: 100 ,y: 100);
+        view.addSubview(label!);
         view.addSubview(joystick)
     }
 
+    func setTextLabel(text: String){
+        joystick.isInAction = true;
+        label?.text = text;
+        setTimeout(delay: 2, block: { () -> Void in
+            // do this stuff after 0.35 seconds
+            self.joystick.isInAction = false;
+        })
+    }
+    func setTimeout(delay:TimeInterval, block:@escaping ()->Void) -> Timer {
+        return Timer.scheduledTimer(timeInterval: delay, target: BlockOperation(block: block), selector: #selector(Operation.main), userInfo: nil, repeats: false)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
---
+++ FWJoystickExempleUITests/FWJoystickExempleUITests.swift
@@ -0,0 +1,121 @@
+//  FWJoystickExempleUITests.swift
+//  FWJoystickExempleUITests
+//
+//  Created by Vincent Durpoix on 15/02/2017.
+//  Copyright © 2017 MOC2. All rights reserved.
+//
+
+import XCTest
+
+class FWJoystickExempleUITests: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testJoystickUp() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let joystickElement = app.otherElements["joystick"]
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.2)).tap()
+        
+        let testLabel = app.textFields["testLabel"]
+        
+        
+        XCTAssertEqual("UP",testLabel.value as! String );
+    
+    }
+    
+    func testJoystickDown() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let joystickElement = app.otherElements["joystick"]
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.8)).tap()
+        
+        
+        let testLabel = app.textFields["testLabel"]
+        
+        
+        XCTAssertEqual("DOWN",testLabel.value as! String );
+        
+    }
+    func testJoystickLeft() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let joystickElement = app.otherElements["joystick"]
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.2, dy: 0.5)).tap()
+        
+        
+        let testLabel = app.textFields["testLabel"]
+        
+        
+        XCTAssertEqual("LEFT",testLabel.value as! String );
+        
+    }
+    
+    func testJoystickRight() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let joystickElement = app.otherElements["joystick"]
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.8, dy: 0.5)).tap()
+        
+        
+        let testLabel = app.textFields["testLabel"]
+        
+        
+        XCTAssertEqual("RIGHT",testLabel.value as! String );
+        
+    }
+    
+    func testIsNotTriggered() {
+        let joystickElement = app.otherElements["joystick"]
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.6, dy: 0.6)).tap()
+        
+        
+        let testLabel = app.textFields["testLabel"]
+        
+        
+        XCTAssertEqual("Hello",testLabel.value as! String );
+    }
+    
+    func testBooleanStop(){
+        let joystickElement = app.otherElements["joystick"]
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.8, dy: 0.5)).tap()
+        
+        
+        let testLabel = app.textFields["testLabel"]
+        
+        
+        XCTAssertEqual("RIGHT",testLabel.value as! String );
+        
+        
+        joystickElement.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.2)).tap()
+
+        
+        XCTAssertEqual("RIGHT",testLabel.value as! String );
+
+    }
+    
+
+    
+    
+}
